e498833283a6e54eb482d7e8300746c330762c31
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2015-12-15 11:20:08 +0100

    Updated softc-API

--- foreign/c/src/softc/softc.c
+++ foreign/c/src/softc/softc.c
@@ -7,7 +7,7 @@ softc_t *softc_init(int argc, char *argv[])
   return softc_private_init(argc, argv);
 }
 
-int softc_registered_storage_driver_count()
+int softc_storage_driver_count()
 {
   return softc_private_registered_storage_driver_count();
 }
