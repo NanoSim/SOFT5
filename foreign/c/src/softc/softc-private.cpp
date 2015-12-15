4d1b392fdf549a97c4549e80dd2e49e8838ab92b
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2015-12-15 12:50:43 +0100

    Fixed soft_init API

--- foreign/c/src/softc/softc-private.cpp
+++ foreign/c/src/softc/softc-private.cpp
@@ -5,9 +5,10 @@
 
 struct _softc_t *softc_private_init(int argc, char *argv[])
 {
-  struct _softc_t *ret;
+  struct _softc_t *ret;  
   ret = (struct _softc_t*) malloc (sizeof (struct _softc_t));
-  ret->app = (void*) new QCoreApplication(argc, argv);
+  ret->app = 0;
+  soft::init(argc, argv);
   return ret;
 }
 
