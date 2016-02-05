63538053debeade3eb6b761fa0ff7bc7c54776a2
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2016-02-05 07:41:15 +0100

    Updated softc-collection and sofftc-datatypes (unfinished). Added datatypes test

--- foreign/c/src/softc/softc-collection.c
+++ foreign/c/src/softc/softc-collection.c
@@ -87,3 +87,32 @@ void softc_collection_free(softc_collection_s *self)
 void softc_collection_register_entity(softc_collection_s *self, const char *label, const softc_entity_t *entity)
 {
 }
+
+void softc_collection_add_dim(softc_collection_s *self, const char *label, const char *description)
+{}
+
+void softc_collection_connection(softc_collection_s *self, const char *subject, const char *predicate, const char *object)
+{}
+
+size_t softc_collection_num_entities(softc_collection_s *self)
+{
+  return 0;
+}
+
+size_t softc_collection_num_dims(softc_collection_s *self)
+{
+  return 0;
+}
+size_t softc_collection_num_relations(softc_collection_s *self)
+{
+  return 0;
+}
+
+size_t softc_collection_num_dim_maps(softc_collection_s *self)
+{
+  return 0;
+}
+
+void softc_collection_get_dimensions(softc_collection_s *self, char ***dimensions)
+{  
+}
