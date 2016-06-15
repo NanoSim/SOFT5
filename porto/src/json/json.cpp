10e4c39db751bc6c02646666c1a783370fd7ebe2
Petter Ronningen <petter.ronningen@gmail.com>
2016-06-15 15:55:35 +0200

    SOFT-39 WIP for making the json plugin plugin work with an actual data model and JSON file as output.

--- porto/src/json/json.cpp
+++ porto/src/json/json.cpp
@@ -1,7 +1,26 @@
 #include "json.h"
 
+#include <string.h>
+#include <algorithm>
+
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QFile>
+
 int softc_plugin_identify( char* name, int maxlen ) {
-  // TODO
+  // TODO: Is this the name we want to return?
+  const char plugin_name[] = "porto-json";
+
+  // Copy no more than the length of the string above or the given maximum length
+  const size_t len = std::min<size_t>(sizeof(plugin_name), maxlen);
+  strncpy(name, plugin_name, len);
+
+  // Ensure that the returned string is 0-terminated, no matter what length constraints it had
+  name[len - 1] = '\0';
+
+  // TODO: Return failure if the target string does not have sufficient space?
+  return SOFTC_STATUS_OK;
 }
 
 int softc_plugin_capabilities() {
@@ -10,9 +29,44 @@ int softc_plugin_capabilities() {
 
 int softc_plugin_load( softc_datamodel_t* datamodel, const char* uri, const char* options ) {
   // TODO
+
 }
 
 int softc_plugin_save( const softc_datamodel_t* datamodel, const char* uri, const char* options) {
-  // TODO
-}
+  
+  // TODO: Some checks for id, version, name and namespace here?
+  // softc_datamodel_get_id
+  // softc_datamodel_get_meta_name
+  // softc_datamodel_get_meta_version
+  // softc_datamodel_get_meta_namespace
+  
+  double dummy;
+  if (!softc_datamodel_get_double(datamodel, "amount", &dummy)) {
+    return SOFTC_STATUS_FAILURE;
+  }
+  
+  QJsonObject root;
+  root["recepie"] = "cheese cake";
 
+  QJsonArray ingredients;
+  ingredients.append("cheese");
+  ingredients.append("flour");
+  ingredients.append("sugar");
+
+  root["ingredients"] = ingredients;
+  
+  root["amount"] = dummy;
+
+  QJsonDocument doc(root);
+
+  QFile f(uri);
+  if (!f.open(QIODevice::WriteOnly)) {
+    return SOFTC_STATUS_FAILURE;
+  }
+
+  // Can f.write fail? If so, return error code
+  f.write(doc.toJson());
+
+  return SOFTC_STATUS_OK;
+}
+ 
--- porto/test/json/test-json.cpp
+++ porto/test/json/test-json.cpp
@@ -3,7 +3,45 @@
 #include <gtest/gtest.h>
 #include <json.h>
 
-TEST(PortoJsonTest, TestCapabilities) 
+#include "json/jsonmodel.h"
+
+TEST(PortoJsonTests, TestIdentify) {
+  {
+    const char expected[] = "porto-json";
+    const int maxlen = sizeof(expected);
+  
+    char name[maxlen + 1]; // String with a canari bird at the end
+    name[maxlen] = '*'; // The function should never overwrite past maxlen!
+
+    int error = softc_plugin_identify(name, maxlen);
+
+    ASSERT_EQ(SOFTC_STATUS_OK, error);
+
+    ASSERT_STREQ("porto-json", name);
+    ASSERT_EQ('*', name[maxlen]);
+  }
+
+  {
+    // Here we construct a too small buffer, but expect the canari to be intact as well
+    // as a null-termination on the actual string
+    const char expected[] = "porto-json";
+    const int maxlen = sizeof(expected) - 2;
+
+    char name[maxlen + 1];
+    name[maxlen] = '*';
+
+    int error = softc_plugin_identify(name, maxlen);
+
+    ASSERT_EQ(SOFTC_STATUS_OK, error);
+    
+    ASSERT_STREQ("porto-js", name);
+    ASSERT_EQ('*', name[maxlen]);
+    ASSERT_EQ('\0', name[maxlen - 1]);
+  }
+}
+
+
+TEST(PortoJsonTests, TestCapabilities) 
 {
   int capabilities = softc_plugin_capabilities();
 
@@ -11,3 +49,14 @@ TEST(PortoJsonTest, TestCapabilities)
   ASSERT_TRUE(capabilities & SOFTC_CAPABILITY_WRITE);
 }
 
+TEST(PortoJsonTests, TestSaveLoad) {
+  // TODO: Construct a datamodel!
+  
+  softc_datamodel_t d;
+  
+  int error = softc_plugin_save(&d, "test.json", "");
+
+  // TODO: Load the saved plugin and check that the data models are the same
+
+  ASSERT_EQ(error, SOFTC_STATUS_OK);
+}
