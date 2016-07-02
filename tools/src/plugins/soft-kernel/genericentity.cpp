791ffdc1a853a456692edbeb3cdb5e527bde7bcd
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2016-07-02 22:33:20 +0200

    Added support for more standard ard types

--- tools/src/plugins/soft-kernel/genericentity.cpp
+++ tools/src/plugins/soft-kernel/genericentity.cpp
@@ -9,32 +9,46 @@
 
 SOFT_BEGIN_NAMESPACE
 
-void appendString(IDataModel* dm, const char* key, QJsonValue const &value)
+namespace {
+  
+static void appendString(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   dm->appendString(key, value.toString().toStdString());
 }
 
-void appendBool(IDataModel* dm, const char* key, QJsonValue const &value)
+static void appendBool(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   dm->appendBool(key, value.toBool());
 }
 
-void appendFloat(IDataModel* dm, const char* key, QJsonValue const &value)
+static void appendFloat(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   dm->appendFloat(key, (float)value.toDouble());
 }
 
-void appendDouble(IDataModel* dm, const char* key, QJsonValue const &value)
+static void appendDouble(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   dm->appendDouble(key, value.toDouble());
 }
 
-void appendInt32(IDataModel* dm, const char* key, QJsonValue const &value)
+static void appendInt32(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   dm->appendInt32(key, value.toInt());
 }
 
-void appendInt32Array(IDataModel* dm, const char* key, QJsonValue const &value)
+static void appendStringArray(IDataModel* dm, const char* key, QJsonValue const &value)
+{
+  assert(value.isArray());
+  auto valueList = value.toArray();    
+  soft::StdStringList array(valueList.size());
+  std::transform(valueList.constBegin(), valueList.constEnd(), array.begin(),
+		 [] (QJsonValue const &value) {
+		   return value.toString().toStdString();
+		 });
+  dm->appendStringArray(key, array);
+}
+
+static void appendInt32Array(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   assert(value.isArray());
   auto valueList = value.toArray();
@@ -46,7 +60,7 @@ void appendInt32Array(IDataModel* dm, const char* key, QJsonValue const &value)
   dm->appendInt32Array(key, array);
 }
 
-void appendDoubleArray(IDataModel* dm, const char* key, QJsonValue const &value)
+static void appendDoubleArray(IDataModel* dm, const char* key, QJsonValue const &value)
 {
   assert(value.isArray());
   auto valueList = value.toArray();
@@ -58,14 +72,98 @@ void appendDoubleArray(IDataModel* dm, const char* key, QJsonValue const &value)
   dm->appendDoubleArray(key, array);
 }
 
-void getInt32(IDataModel const* dm, const char* key, QJsonValue &value)
+static void appendDoubleArray2D(IDataModel* dm, const char* key, QJsonValue const &value)
+{
+  assert(value.isArray());
+  auto valueList2D = value.toArray();
+
+  soft::StdDoubleArray2D array2D(valueList2D.size());
+  foreach(auto value2, valueList2D) {    
+    assert(value2.isArray());
+    auto valueList = value2.toArray();
+    soft::StdDoubleArray array(valueList.size());
+    std::transform(valueList.constBegin(), valueList.constEnd(), array.begin(),
+		   [] (QJsonValue const &value) {
+		     return value.toDouble();
+		   });
+    array2D.push_back(array);
+  }
+  dm->appendDoubleArray2D(key, array2D);
+}
+
+static void getString(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdString v;
+  dm->getString(key, v);
+  value = QJsonValue(QString::fromStdString(v));
+}
+
+static void getBool(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdBool v;
+  dm->getBool(key, v);
+  value = QJsonValue(v);
+}
+
+static void getFloat(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdFloat v;
+  dm->getFloat(key, v);
+  value = QJsonValue((double)v);
+}
+
+static void getDouble(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdDouble v;
+  dm->getDouble(key, v);
+  value = QJsonValue(v);
+}
+
+static void getStringArray(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdStringList v;
+  dm->getStringArray(key, v);
+  QJsonArray jsonArray;
+  for (auto i: v) {
+    jsonArray.push_back(QString::fromStdString(i));
+  }
+  value = jsonArray;
+}
+
+static void getDoubleArray(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdDoubleArray v;
+  dm->getDoubleArray(key, v);
+  QJsonArray jsonArray;
+  for(auto i: v) {
+    jsonArray.push_back(i);
+  }
+  value = jsonArray;
+}
+
+static void getDoubleArray2D(IDataModel const* dm, const char* key, QJsonValue &value)
+{
+  soft::StdDoubleArray2D v;
+  dm->getDoubleArray2D(key, v);
+  QJsonArray jsonArray;
+  for(auto i: v) {
+    QJsonArray jsonArray2;
+    for(auto i2: i) {
+      jsonArray2.push_back(i2);
+    }
+    jsonArray.push_back(jsonArray2);
+  }
+  value = jsonArray;
+}
+
+static void getInt32(IDataModel const* dm, const char* key, QJsonValue &value)
 {
   soft::StdInt v;
   dm->getInt32(key, v);
   value = QJsonValue(v);
 }
 
-void getInt32Array(IDataModel const* dm, const char* key, QJsonValue &value)
+static void getInt32Array(IDataModel const* dm, const char* key, QJsonValue &value)
 {
   soft::StdIntArray stdIntArray;
   dm->getInt32Array(key, stdIntArray);
@@ -76,25 +174,40 @@ void getInt32Array(IDataModel const* dm, const char* key, QJsonValue &value)
   value = jsonArray;
 }
 
+}
+
 typedef void(*GetFnPtr)(IDataModel const*,const char*,QJsonValue&);
 typedef void(*AppendFnPtr)(IDataModel*,const char*,QJsonValue const&);
 using PropertyClass = QPair<QString,int>;
 static std::map<PropertyClass, GetFnPtr> getMap = {
-  {qMakePair<QString,int>("int", 0), &getInt32},
-  {qMakePair<QString,int>("int32", 0), &getInt32},
+  {qMakePair<QString,int>("int",     0), &getInt32},
+  {qMakePair<QString,int>("int32",   0), &getInt32},
   {qMakePair<QString,int>("integer", 0), &getInt32},
-  {qMakePair<QString,int>("int", 1), &getInt32Array}
+  {qMakePair<QString,int>("int",     1), &getInt32Array},
+  {qMakePair<QString,int>("int32",   1), &getInt32Array},
+  {qMakePair<QString,int>("integer", 1), &getInt32Array},
+  {qMakePair<QString,int>("string",  0), &getString},
+  {qMakePair<QString,int>("string",  1), &getStringArray},
+  {qMakePair<QString,int>("bool",    0), &getBool},
+  {qMakePair<QString,int>("float",   0), &getFloat},
+  {qMakePair<QString,int>("double",  0), &getDouble},
+  {qMakePair<QString,int>("double",  1), &getDoubleArray},
+  {qMakePair<QString,int>("double",  2), &getDoubleArray2D}
 };
+
 static std::map<PropertyClass, AppendFnPtr> appendMap = {
-  {qMakePair<QString,int>("int", 0), &appendInt32},
-  {qMakePair<QString,int>("int32", 0), &appendInt32},
+  {qMakePair<QString,int>("int",     0), &appendInt32},
+  {qMakePair<QString,int>("int32",   0), &appendInt32},
   {qMakePair<QString,int>("integer", 0), &appendInt32},
-  {qMakePair<QString,int>("int", 1), &appendInt32Array},
-  {qMakePair<QString,int>("string", 0), &appendString},
-  {qMakePair<QString,int>("bool", 0), &appendBool},
-  {qMakePair<QString,int>("float", 0), &appendFloat},
-  {qMakePair<QString,int>("double", 0), &appendDouble},
-  {qMakePair<QString,int>("double", 1), &appendDoubleArray}
+  {qMakePair<QString,int>("int",     1), &appendInt32Array},
+  {qMakePair<QString,int>("int32",   1), &appendInt32Array},
+  {qMakePair<QString,int>("integer", 1), &appendInt32Array},
+  {qMakePair<QString,int>("string",  0), &appendString},
+  {qMakePair<QString,int>("string",  1), &appendStringArray},
+  {qMakePair<QString,int>("bool",    0), &appendBool},
+  {qMakePair<QString,int>("float",   0), &appendFloat},
+  {qMakePair<QString,int>("double",  0), &appendDouble},
+  {qMakePair<QString,int>("double",  2), &appendDoubleArray2D}
 };
 
 struct GenericEntity :: Private
