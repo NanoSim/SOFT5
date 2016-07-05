ee35a997029fbe5115287ac94a15d56ba1b9cc89
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2016-07-05 07:12:01 +0200

    Updated genudf to read the data from the Porto database

--- porto/src/udfgenerator/genudf.js
+++ porto/src/udfgenerator/genudf.js
@@ -1,15 +1,31 @@
 #!/usr/bin/env softshell
 
+var entity = require('porto.entity');
+porto.ChemkinReaction = entity.using('chemkinReaction', 'eu.nanosim.vasp', '0.1');
+
 __main__ = function (args)
 {
     try {
+        var inputUUID = args[1];
+        var collection  = new porto.Collection(inputUUID);
+        var storage = new porto.Storage("mongo2", "mongodb://localhost", "db=porto;coll=demo");
+        storage.load(collection);
+        
+        var chemkinCollectionUUID = collection.getUuid("chemKinData");
+        var chemkinCollecton = new porto.Collection(chemkinCollectionUUID);
+        storage.load(chemkinCollecton);
+        
+        var reactionUUID = chemkinCollecton.getUuid("reaction_1");
+        var reaction = porto.ChemkinReaction(reactionUUID);
+        storage.load(reaction);
+        
 	var controller = require('soft.mvc').create({
 	    model: {
 		min_cutoff: 1e-6,
 		dp : 0.00001,
 		k0 : 2.5,
 		Temp: 1173.,
-		EA: 70000.,
+		EA: reaction.Ea, /* Read value from coming for the Porto database */
 		Gas_Const: 8.314,
 		S0Eq: "6.0 * 0.2066 * vol_frac_solid / dp", 
 		kEq: "k0 * exp(-EA/(Gas_Const*Temp))",
