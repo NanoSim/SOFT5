9ce37c9ec2a4534370819256161f05f0124f7c99
Jesper Friis <jesper.friis@sintef.no>
2017-02-10 21:27:51 +0100

    Added python tests for string lists

--- foreign/python/test/softpy/softpy-test-string-list.py
+++ foreign/python/test/softpy/softpy-test-string-list.py
@@ -3,3 +3,15 @@
 from __future__ import print_function
 
 import softpy
+
+strlist = softpy.string_list_create()
+softpy.string_list_append_cstr(strlist, 'first')
+softpy.string_list_append_cstr(strlist, 'second')
+softpy.string_list_append_cstr(strlist, 'third')
+
+assert softpy.string_list_count(strlist) == 3
+assert softpy.from_softc_string(softpy.string_list_first(strlist)) == 'first'
+assert softpy.from_softc_string(softpy.string_list_last(strlist)) == 'third'
+assert softpy.from_softc_string(softpy.string_at(strlist, 0)) == 'first'
+assert softpy.from_softc_string(softpy.string_at(strlist, 1)) == 'second'
+assert softpy.from_softc_string(softpy.string_at(strlist, 2)) == 'third'
